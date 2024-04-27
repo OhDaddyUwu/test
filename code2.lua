dfd56c6cda9709da804c92a0a3862448f7c40f9c
OhDaddyUwu <144573349+OhDaddyUwu@users.noreply.github.com>
2024-04-27 13:21:39 +0100

    Update code2.lua

--- code2.lua
+++ code2.lua
@@ -1,5 +1,13 @@
-while true do
-print("test")
+local text = "Rainbow Text!"
+local rainbowColors = "0123456789abcdef"
+local textLength = #text
+local startX = 1
+local startY = 1
 
-print("test2")
+for i = 1, textLength do
+    local colorIndex = (i - 1) % #rainbowColors + 1
+    local textColor = rainbowColors:sub(colorIndex, colorIndex)
+    term.blit(text:sub(i, i), textColor, "0")
 end
+
+term.setCursorPos(startX, startY + 1)
