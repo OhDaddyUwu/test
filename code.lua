d2ea03e8b082cf67dc64ca04805388b727eea09b
OhDaddyUwu <144573349+OhDaddyUwu@users.noreply.github.com>
2024-04-27 13:23:26 +0100

    Update code.lua

--- code.lua
+++ code.lua
@@ -1,5 +1,14 @@
-while true do
-print("test")
-wait(1)
-print("test2")
+local rainbowColors = "0123456789abcdef"
+
+local width, height = term.getSize()
+
+local startX = 1
+local startY = 1
+
+for y = 1, height do
+    for x = 1, width do
+        local colorIndex = ((x - 1) + (y - 1)) % #rainbowColors + 1
+        local textColor = rainbowColors:sub(colorIndex, colorIndex)
+        term.blit(" ", textColor, "0")
+    end
 end
