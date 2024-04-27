cb50b89d1ef3319b87f9b9d0097f5a57c3a51da1
OhDaddyUwu <144573349+OhDaddyUwu@users.noreply.github.com>
2024-04-27 13:25:29 +0100

    Update 3.lua

--- 3.lua
+++ 3.lua
@@ -1,4 +1,17 @@
-while true do
-print("test")
-print("test2")
+local rainbowColors = "0123456789abcdef"
+
+local monitor = peripheral.find("monitor")
+
+if monitor then
+    local width, height = monitor.getSize()
+    
+    for y = 1, height do
+        for x = 1, width do
+            local colorIndex = ((x - 1) + (y - 1)) % #rainbowColors + 1
+            local textColor = rainbowColors:sub(colorIndex, colorIndex)
+            monitor.blit(" ", textColor, "0")
+        end
+    end
+else
+    print("No monitor found.")
 end
